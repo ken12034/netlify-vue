551bfba2c8ebcdf5ae22e4c8e3149204851ff7c4
Ken <ken12034@gmail.com>
2021-04-20 23:18:36 +0800

    add: data-netlify-honeypot="bot-field"

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="landingpage">
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true"
+    data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact">
         <p>
             <label>Your Name: <input type="text" name="name" /></label>   
