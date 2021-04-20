eeb6799233d44171792661957cb0225fa9fd95ec
Ken <ken12034@gmail.com>
2021-04-20 22:34:43 +0800

    add: form test

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="landingpage">
-
+    <form name="contact" netlify>
+        <p>
+            <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
   </div>
 </template>
 
