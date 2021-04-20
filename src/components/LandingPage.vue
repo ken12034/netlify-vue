b3bfa489afc171b5919c970275768371047e4c7c
Ken <ken12034@gmail.com>
2021-04-20 23:01:05 +0800

    add input name form name

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="landingpage">
     <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact">
         <p>
             <label>Your Name: <input type="text" name="name" /></label>   
         </p>
         <p>
             <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
         </p>
         <p>
             <button type="submit">Send</button>
